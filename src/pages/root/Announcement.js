81b1647e22cb762ba3b5504ba15c85988e737784
Oikon.cash <admin@oikon.cash>
2020-06-11 17:58:28 -0400

    IEO url

--- src/pages/root/Announcement.js
+++ src/pages/root/Announcement.js
@@ -4,7 +4,7 @@ import styles from './root.module.scss';
 export default () => {
   return (
     <div className={styles.announcement}>
-      <a href="https://support.hoo.com/hc/en-us/articles/900001258163">
+      <a href="https://hoo.com/labs/detail/oks">
         Announcement: Oikos IEO on Hoo.com from Jun 12 to Jun 14 🚀
       </a>
       <div className="x"></div>
