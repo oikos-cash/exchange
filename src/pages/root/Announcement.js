f5f3ba4eff23bd8b7e41d8fb463730d10c18e8c1
Kevin Holder <kevholderx@gmail.com>
2020-06-12 09:24:50 -0400

    ieo url #2

--- src/pages/root/Announcement.js
+++ src/pages/root/Announcement.js
@@ -4,7 +4,7 @@ import styles from './root.module.scss';
 export default () => {
   return (
     <div className={styles.announcement}>
-      <a href="https://hoo.com/labs/detail/oks">
+      <a href="https://hoo.com/labs/detail/oks2">
         Announcement: Oikos IEO on Hoo.com from Jun 12 to Jun 14 🚀
       </a>
       <div className="x"></div>
